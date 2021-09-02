f815b130169fa6a5679ad5dc4bd23c625aeab71f
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-09-02 19:00:43 +0430

    feat : get_stat function added

--- boardroom/functions.py
+++ boardroom/functions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Boardroom functions."""
-
+from .params import *
 import requests
 
 def get_protocol(cname):
@@ -16,6 +16,19 @@ def get_voter(cname,address):
     pass
 
 def get_stat():
-    pass
+    """
+    Get global platform stats.
+
+    :return: data as dict
+    """
+    try:
+        response = requests.get(API_BASE + "/stats")
+        if response.status_code == 200:
+            data_json = response.json()
+            return data_json["data"]
+        return None
+    except Exception:
+        return None
+
 
 
---
+++ boardroom/params.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Boardroom params."""
+
+API_BASE = "https://api.boardroom.info/v1"
+
+
