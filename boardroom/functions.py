472ad3ad3223d1c7c951340119bed16f067fc737
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-09-02 18:54:33 +0430

    feat : functions abstract added

---
+++ boardroom/functions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Boardroom functions."""
+
+import requests
+
+def get_protocol(cname):
+    pass
+
+def get_proposal(cname,ref_id):
+    pass
+
+def get_vote(address,ref_id):
+    pass
+
+def get_voter(cname,address):
+    pass
+
+def get_stat():
+    pass
+
+
