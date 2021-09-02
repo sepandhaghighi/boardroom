273dcc66a1bd7d46cebacf35e83fa45448fafc55
sepandhaghighi <sepand.haghighi@yahoo.com>
2021-09-03 02:19:11 +0430

    feat : get_protocol function added

--- boardroom/functions.py
+++ boardroom/functions.py
@@ -3,8 +3,26 @@
 from .params import *
 import requests
 
-def get_protocol(cname):
-    pass
+def get_protocol(cname="all"):
+    """
+    Get protocols details.
+
+    :param cname: protocol cname
+    :type cname: str
+    :return: data as dict
+    """
+    try:
+        api = API_BASE + "/protocols/{0}".format(cname)
+        if cname.lower() == "all":
+            api = API_BASE + "/protocols"
+        response = requests.get(api)
+        if response.status_code == 200:
+            data_json = response.json()
+            return data_json["data"]
+        return None
+    except Exception:
+        return None
+
 
 def get_proposal(cname,ref_id):
     pass
